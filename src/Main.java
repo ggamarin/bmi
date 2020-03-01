9eba9be9d7158c9e41f884c120a81d3a004cfca6
ggamarin <60188645+ggamarin@users.noreply.github.com>
2020-03-01 12:01:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double weight = 76;
-        double height = 1.76;
+        double height = 1.76; //height in meters
         double bmi = service.calculate(height, weight);
         System.out.printf("BMI is " + bmi);
 
